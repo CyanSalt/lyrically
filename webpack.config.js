667b745af9f33bb6a49e3c86e581683488ac5138
CyanSalt <c.salt@hotmail.com>
2020-03-04 11:59:04 +0800

    Update bundling configuration

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,15 @@ const path = require('path')
 const webpack = require('webpack')
 const MiniCSSExtractPlugin = require('mini-css-extract-plugin')
 const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const app = require('./package')
+
+function externalizeAllDenpendencies(dependencies) {
+  if (!dependencies) return {}
+  return Object.keys(dependencies).reduce((externals, dependency) => {
+    externals[dependency] = `commonjs2 ${dependency}`
+    return externals
+  }, {})
+}
 
 module.exports = {
   target: 'electron-renderer',
@@ -13,14 +22,13 @@ module.exports = {
   node: {
     __dirname: false,
   },
-  entry: {
-    renderer: path.resolve(__dirname, 'renderer/index.js'),
-  },
+  entry: path.resolve(__dirname, 'renderer/index.js'),
   output: {
     path: path.resolve(__dirname, 'renderer/build/'),
-    filename: '[name].js',
+    filename: 'index.js',
     libraryTarget: 'commonjs2',
   },
+  externals: externalizeAllDenpendencies(app.dependencies),
   resolve: {
     extensions: ['.js', '.vue'],
     alias: {
@@ -50,7 +58,7 @@ module.exports = {
   },
   plugins: [
     new MiniCSSExtractPlugin({
-      filename: 'renderer.css',
+      filename: 'index.css',
     }),
     new VueLoaderPlugin(),
     new webpack.ProgressPlugin(),
