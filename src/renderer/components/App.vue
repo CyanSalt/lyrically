a3bcfb79c659a3e24e8e4772d29af399bd587101
CyanSalt <c.salt@hotmail.com>
2025-03-04 21:38:55 +0800

    fix: reduce notch size

--- src/renderer/components/App.vue
+++ src/renderer/components/App.vue
@@ -305,7 +305,7 @@ function toggleCollapsed() {
 watchEffect(() => {
   if (isNotchWindow) {
     const iconHeight = notchAreaHeight / (1 + 1.75)
-    const compactHeight = Math.ceil((3.75 + 5 + 2) * iconHeight)
+    const compactHeight = Math.ceil((1 + 1.75 + 0.5 + 2 + 0.5 + 1.75 + 1) * iconHeight)
     worldBridge.setBounds({
       height: isCollapsed ? notchAreaHeight : (isCompact ? compactHeight : notchAreaHeight * 6),
     })
@@ -806,8 +806,8 @@ watchEffect(() => {
   .app.is-compact & {
     justify-content: flex-start;
     padding-inline: var(--icon-size);
-    padding-top: calc(#{1.75 + 2 * 1} * var(--icon-size));
-    padding-bottom: calc(#{3 + 2 * 1} * var(--icon-size));
+    padding-top: calc(#{1 + 1.75 + 0.5} * var(--icon-size));
+    padding-bottom: calc(#{0.5 + 1.75 + 1} * var(--icon-size));
     font-weight: 500;
     font-size: max(1em, 14px);
     animation: none;
@@ -877,7 +877,7 @@ watchEffect(() => {
   width: 5em;
   height: 5em;
   color: black;
-  transition: color var(--effect-duration);
+  transition: color var(--effect-duration), width var(--effect-duration), height var(--effect-duration);
   cursor: pointer;
   container-type: size;
   &::before {
@@ -893,8 +893,8 @@ watchEffect(() => {
     color: white;
   }
   .app.is-compact & {
-    width: 3em;
-    height: 3em;
+    width: 1.75em;
+    height: 1.75em;
   }
   :deep(.lucide) {
     z-index: 1;
@@ -915,6 +915,13 @@ watchEffect(() => {
   display: flex;
   flex-direction: column;
   gap: #{0.75em * 0.25};
+  .app.is-compact & {
+    flex: auto;
+    flex-direction: row;
+    gap: #{0.75em * 0.5};
+    align-items: center;
+    min-width: 0;
+  }
 }
 .music-name {
   appearance: none;
@@ -931,6 +938,10 @@ watchEffect(() => {
     font-style: italic;
     transition: color var(--effect-duration);
   }
+  .app.is-compact & {
+    flex: 0 1 auto;
+    min-width: 0;
+  }
 }
 .music-detail {
   display: flex;
@@ -942,7 +953,10 @@ watchEffect(() => {
   transition: color var(--effect-duration);
   cursor: pointer;
   .app.is-compact & {
+    flex: 0 2 auto;
+    min-width: 0;
     font-size: calc(1em - 1px);
+    overflow: hidden;
   }
 }
 .vendor-area {
@@ -953,6 +967,9 @@ watchEffect(() => {
   justify-content: flex-end;
   align-items: center;
   font-size: var(--icon-size);
+  .app.is-compact & {
+    flex: none;
+  }
 }
 .vendor-list {
   display: flex;
